7b6122b16b626606905486dd0abda1391b1342a6
Osvaldo Cornelio de la Rosa <osvaldocr343@gmail.com>
2025-10-05 23:52:44 -0600

    Footer en php
    Seccion 29 min 12:00

--- index.php
+++ index.php
@@ -237,20 +237,6 @@
 
     </div>
 
-    <footer class="footer seccion">
-      <div class="contenedor contenido-footer">
-        <nav class="navegacion">
-          <a href="nosotros.php">Nosotros</a>
-          <a href="anuncios.php">Anuncios</a>
-          <a href="blog.php">Blog</a>
-          <a href="contacto.php">Contacto</a>
-        </nav>
-      </div>
-      <p class="copyright">
-        &copy; 2025 Bienes Raices. Todos los derechos reservados.
-      </p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-  </body>
-</html>
+<?php 
+  include 'includes/templates/footer.php'; 
+?>
